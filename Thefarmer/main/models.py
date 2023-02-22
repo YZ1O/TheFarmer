365516ccd80b67841b4786b36ff0e74c48d6e5f9
Yazeed <Yazeedrq9@gmail.com>
2023-02-22 13:24:37 +0300

    create project and main app

---
+++ Thefarmer/main/models.py
@@ -0,0 +1,10 @@
+from django.db import models 
+from django.contrib.auth.models import User
+
+class customer(models.Model):
+    pass
+class farmer(models.Model):
+    pass
+
+class product(models.Model):
+    pass
